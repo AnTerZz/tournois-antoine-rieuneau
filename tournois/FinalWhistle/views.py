e134e964d08f9163aeccf2741ccc9ed9034b359e
Antoine Rieuneau <antoine.rieuneau@me.com>
2023-04-15 13:40:06 +0200

    Added bootstrap studio templates Linked the Index view to the template

--- tournois/FinalWhistle/views.py
+++ tournois/FinalWhistle/views.py
@@ -7,4 +7,7 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return all the tournaments"""
-        return Tournament.objects.order_by('name')
+        return Tournament.objects.order_by('name')[:3]
+    
+def custom_404(request, exception):
+    return render(request, '404.html', status=404)

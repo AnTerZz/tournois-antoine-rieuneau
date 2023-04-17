e7de85111d486e7c4a84d848c099ce2702c7b0d7
Antoine Rieuneau <antoine.rieuneau@me.com>
2023-04-17 15:40:23 +0200

    Working login system using the generic views

--- tournois/tournois/settings.py
+++ tournois/tournois/settings.py
@@ -124,4 +124,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'FinalWhistle/index'
-LOGOUT_REDIRECT_URL = 'index'
+LOGOUT_REDIRECT_URL = 'FinalWhistle/index'

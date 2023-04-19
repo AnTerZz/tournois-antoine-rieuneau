f4ab75a03835a7d4906b8c5cf46d51b9218febf6
Antoine Rieuneau <antoine.rieuneau@me.com>
2023-04-19 22:35:58 +0200

    Added comments inline to game in admin page

--- tournois/FinalWhistle/admin.py
+++ tournois/FinalWhistle/admin.py
@@ -10,6 +10,10 @@ class GameInline(admin.TabularInline):
 class TeamInline(admin.TabularInline):
     model = Team
     extra = 1
+    
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 1
 
 
 class PouleAdmin(admin.ModelAdmin):
@@ -24,13 +28,16 @@ class PouleAdmin(admin.ModelAdmin):
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('user', 'body', 'game', 'date')
-    #list_filter = ('date')
     search_fields = ('user', 'body')
 
+class GameAdmin(admin.ModelAdmin):
+    inlines=[CommentInline]
+    list_display = ('date', 'location', 'home_team', 'away_team','poule','score')
+    search_fields = ('location', 'home_team','away_team','poule')
 
 
 # Register your models here.
 admin.site.register(Tournament)
 admin.site.register(Team)
-admin.site.register(Game)
+admin.site.register(Game, GameAdmin)
 admin.site.register(Poule, PouleAdmin)

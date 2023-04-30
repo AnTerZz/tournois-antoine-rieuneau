68cb0dd513a8ae6aa83450b9cd1a58f2d8ae1c32
Antoine Rieuneau <antoine.rieuneau@me.com>
2023-04-30 14:03:21 +0200

    Updated goals scatter plot Modified plot to suit style Included the plot in the tournament page Updated view/urls accordingly

--- tournois/FinalWhistle/views.py
+++ tournois/FinalWhistle/views.py
@@ -71,30 +71,6 @@ def custom_404(request, exception):
 
 
 
-from django.views.generic import TemplateView
-from chartjs.views.lines import BaseLineChartView
-
-
-class LineChartJSONView(BaseLineChartView):
-    def get_labels(self):
-        """Return 7 labels for the x-axis."""
-        return ["January", "February", "March", "April", "May", "June", "July"]
-
-    def get_providers(self):
-        """Return names of datasets."""
-        return ["Central", "Eastside", "Westside"]
-
-    def get_data(self):
-        """Return 3 datasets to plot."""
-
-        return [[75, 44, 92, 11, 44, 95, 35],
-                [41, 92, 18, 3, 73, 87, 92],
-                [87, 21, 94, 3, 90, 13, 65]]
-
-
-line_chart = TemplateView.as_view(template_name='FinalWhistle/line_chart.html')
-
-
 from django.shortcuts import render
 from .models import Tournament, Game, Team
 
